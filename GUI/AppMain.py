0882468bfeadac934ee58e1394b3f86210d73e7f
chankou <49492862+chankou001@users.noreply.github.com>
2022-06-18 16:39:46 +0900

    tre

---
+++ GUI/AppMain.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+
+class AppMain():
+    def __init__(self, root):
+        self.root = root
+
+    pass
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = AppMain(root)
+
+    
